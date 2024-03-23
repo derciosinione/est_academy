69dd24c188ef14bfeada1550b9a7f8799e04e6aa
derciosinione <derciosinione@gmail.com>
2024-03-23 23:40:02 +0000

    Created student cards

---
+++ paginas/site/js/student.js
@@ -0,0 +1,18 @@
+let moreOption = document.getElementById("more-option");
+
+function toggleMoreOption() {
+    if (moreOptionIsVisible()) hideMoreOption();
+    else showMoreOption();
+}
+
+function moreOptionIsVisible(){
+    return moreOption.style.display === "block";
+}
+
+function hideMoreOption(){
+    moreOption.style.display = "none";
+}
+
+function showMoreOption(){
+    moreOption.style.display = "block";
+}
