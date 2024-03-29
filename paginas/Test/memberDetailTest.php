04dea28f1c33b19c0c64c3afc561ae7518c2f588
derciosinione <derciosinione@gmail.com>
2024-03-29 16:54:06 +0000

    .

---
+++ paginas/Test/memberDetailTest.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<?php
+
+include "displayMessageIfExists.php" ?>
+
+<header>Member Detail</header>
+
+<?php
+
+require_once '../../Services/userService.php';
+
+    require_once __DIR__ . "/../../Models/Users/userModel.php";
+    /** @var userModel $loggedUser */
+
+    if(isset($_SESSION['loggedUser'])){
+        $loggedUser = unserialize($_SESSION['loggedUser']);
+
+        echo "<h1>Logged User</h1>";
+        echo "<h3>Email: $loggedUser->email</h3>";
+        echo "<hr>";
+    }
+
+    if (isset($_GET['userId'])){
+        $userId = $_GET['userId'];
+
+        $userService = new userService();
+        $createdManager =  $userService->getUserById($userId);
+
+        echo "Manager created successfully!<br>";
+        echo "Manager ID: " . $createdManager->id . "<br>";
+        echo "Manager Name: " . $createdManager->name . "<br>";
+        echo "Manager Email: " . $createdManager->email . "<br>";
+    }else{
+        echo "<h1>Provide the user id to show details</h1>";
+    }
+?>
+
+<div><a href="adminTest.php">Back</a></div> <br>
+<a href="CreateMemberTest.php">Create Member</a>
+</body>
+</html>
--- paginas/circularAvatar.php
+++ paginas/circularAvatar.php
@@ -4,9 +4,11 @@ if (isset($_SESSION['loggedUser'])) {
     $loggedUser = unserialize($_SESSION['loggedUser']);
 
     echo <<<HTML
-        <div class="avatar">
-            <img alt="" src="$loggedUser->avatarUrl">
-        </div> 
+        <a href="account-profile.php">
+            <div class="avatar">
+                <img alt="" src="$loggedUser->avatarUrl">
+            </div> 
+        </a>
 HTML;
 
 }
