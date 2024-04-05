09f3ee5cb23db85709153199ecc4bef32335861b
derciosinione <derciosinione@gmail.com>
2024-04-05 03:46:36 +0100

    Created setting > Side bar menu

--- paginas/settings.php
+++ paginas/settings.php
@@ -6,6 +6,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <link href="main.css" rel="stylesheet">
     <link href="components.css" rel="stylesheet">
+    <link href="settings.css" rel="stylesheet">
     <title>Settings</title>
 </head>
 <body>
@@ -39,6 +40,34 @@
         <!-- MAIN BODY -->
         <div class="main-body">
 
+            <div class="setting-aside">
+                <div class="user-avatar">
+                    <img class="img-cover"
+                         src="https://images.unsplash.com/photo-1589156191108-c762ff4b96ab?q=80&w=3086&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+                </div>
+                <div class="pl20 pr20 mt15 pb10">
+                    <h3>Angela de Jesus Derone</h3>
+                    <p class="blackOpacity mt5 smallText"><i class="fas fa-id-card"></i> Administrador</p>
+                </div>
+
+                <div class="horizontal-line"></div>
+
+                <nav>
+                    <ul>
+                        <li class="active-settings-menu"><a href="#">Conta</a></li>
+                        <li><a href="#">Senha</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Usuários</a></li>
+                        <li><a href="#">Categorias</a></li>
+                        <li><a href="#">Estados</a></li>
+                    </ul>
+                </nav>
+
+            </div>
+
+            <section>
+                User info section
+            </section>
 
         </div>
 
